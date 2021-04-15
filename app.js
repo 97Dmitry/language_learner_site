15a3a6a58db7319e7df938a267bcd199aa013ec9
97Dmitry <dart_veyder_1997@mail.ru>
2021-04-15 16:01:31 +0600

    For previous commit

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ require('dotenv').config()
 const app = express()
 
 app.use(express.json())
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
+app.use((request, response, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 // ROUTING
 app.use("/api", require("./routes/user.routes"))
 app.use("/api", require("./routes/word.routes"))
 app.use("/api", require("./routes/auth.routes"))
+app.use("/api", require("./routes/permissions.routes"))
 
 const PORT = process.env.PORT || 7000
 const start = () => {
