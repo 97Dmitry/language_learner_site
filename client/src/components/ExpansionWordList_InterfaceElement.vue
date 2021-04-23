538ccde2331d15f11bc52c4ad21eb38f23090d47
97Dmitry <dart_veyder_1997@mail.ru>
2021-04-23 16:01:36 +0600

    Created pre-alpha version-2 ExpansionWordsList page

---
+++ client/src/components/ExpansionWordList_InterfaceElement.vue
@@ -0,0 +1,122 @@
+<template>
+  <div style="display: flex">
+    <div class="example_table_verb" style="display: flex; margin-right: 40px">
+      <button
+        style="width: 120px; height: 45px; margin-right: 20px"
+        v-on:click.prevent="addVerbLine"
+      >
+        Add new line for verb
+      </button>
+      <div>
+        <ul id="verb_table">
+          <li>
+            <div class="input-field inline">
+              <input id="verb_inline" type="text" class="validate" />
+              <label for="verb_inline">Verb</label>
+              <small class="helper-text invalid"></small>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="example_table_noun" style="display: flex; margin-right: 40px">
+      <button
+        style="width: 120px; height: 45px; margin-right: 20px"
+        v-on:click.prevent="addNounLine"
+      >
+        Add new line for noun
+      </button>
+      <div>
+        <ul id="noun_table">
+          <li>
+            <div class="input-field inline">
+              <input id="noun_inline" type="text" class="validate" />
+              <label for="noun_inline">Noun</label>
+              <small class="helper-text invalid"></small>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div
+      class="example_table_general"
+      style="display: flex; margin-right: 40px"
+    >
+      <button
+        style="width: 120px; height: 45px; margin-right: 20px"
+        v-on:click.prevent="addGeneralLine"
+      >
+        Add new line for general
+      </button>
+      <div>
+        <ul id="general_table">
+          <li>
+            <div class="input-field inline">
+              <input id="general_inline" type="text" class="validate" />
+              <label for="general_inline">General</label>
+              <small class="helper-text invalid"></small>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ExpansionWordList_InterfaceElement",
+  data() {
+    return {
+      verbLine: 1,
+      nounLine: 1,
+      generalLine: 1,
+    };
+  },
+  methods: {
+    addVerbLine() {
+      if (this.verbLine < 3) {
+        document.getElementById("verb_table").insertAdjacentHTML(
+          "afterend",
+          `<li style="list-style-type: none"><div class="input-field inline">
+          <input id="verb_inline_${this.verbLine}" type="text" class="validate" />
+          <label for="verb_inline_${this.verbLine}">Another verb</label>
+          <small class="helper-text invalid"></small>
+        </div></li>`
+        );
+        this.verbLine += 1;
+      }
+    },
+    addNounLine() {
+      if (this.nounLine < 3) {
+        document.getElementById("noun_table").insertAdjacentHTML(
+          "afterend",
+          `<li style="list-style-type: none"><div class="input-field inline">
+          <input id="noun_inline_${this.nounLine}" type="text" class="validate" />
+          <label for="noun_inline_${this.nounLine}">Another noun</label>
+          <small class="helper-text invalid"></small>
+        </div></li>`
+        );
+        this.nounLine += 1;
+      }
+    },
+    addGeneralLine() {
+      if (this.generalLine < 3) {
+        document.getElementById("general_table").insertAdjacentHTML(
+          "afterend",
+          `<li style="list-style-type: none"><div class="input-field inline">
+          <input id="general_inline${this.generalLine}" type="text" class="validate" />
+          <label for="general_inline${this.generalLine}">Another general</label>
+          <small class="helper-text invalid"></small>
+        </div></li>`
+        );
+        this.generalLine += 1;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- client/src/views/ExpansionWordsList.vue
+++ client/src/views/ExpansionWordsList.vue
@@ -1,114 +1,41 @@
 <template>
   <div class="expansionWordsList">
-    <div class="col s12">
-      Input word that you want to add:
-      <div class="input-field inline">
-        <input id="email_inline" type="email" class="validate" />
-        <label for="email_inline">Word</label>
-        <small class="helper-text invalid"></small>
-      </div>
-    </div>
-    <div style="">
-      <div class="example_table_verb">
-        <button
-          style="width: 20px; height: 20px; margin-right: 20px"
-          v-on:click="addVerbLine"
-        >
-          +
-        </button>
+    <h1 class="center title_page">Add new word for study</h1>
+    <form>
+      <div class="col s12">
+        Input word that you want to add:
         <div class="input-field inline">
-          <input id="verb_inline" type="email" class="validate" />
-          <label for="verb_inline">Verb</label>
+          <input id="learning_word_inline" type="text" class="validate" />
+          <label for="learning_word_inline">Word</label>
           <small class="helper-text invalid"></small>
         </div>
       </div>
-
-      <div class="example_table_noun">
-        <button
-          style="width: 20px; height: 20px; margin-right: 20px"
-          v-on:click="addNounLine"
-        >
-          +
-        </button>
-        <div class="input-field inline">
-          <input id="noun_inline" type="email" class="validate" />
-          <label for="noun_inline">Noun</label>
-          <small class="helper-text invalid"></small>
-        </div>
-      </div>
-
-      <div class="example_table_general">
-        <button
-          style="width: 20px; height: 20px; margin-right: 20px"
-          v-on:click="addGeneralLine"
-        >
-          +
-        </button>
-        <div class="input-field inline">
-          <input id="general_inline" type="email" class="validate" />
-          <label for="general_inline">General</label>
-          <small class="helper-text invalid"></small>
-        </div>
-      </div>
-    </div>
+      <ExpansionWordList_InterfaceElement />
+      <button>Add to study</button>
+    </form>
   </div>
 </template>
 
 <script>
+import ExpansionWordList_InterfaceElement from "@/components/ExpansionWordList_InterfaceElement";
+
 export default {
   name: "ExpansionWordsList",
   data() {
-    return {
-      verbLine: 0,
-      nounLine: 0,
-      generalLine: 0,
-    };
+    return {};
   },
-  methods: {
-    addVerbLine() {
-      if (this.verbLine < 3) {
-        document
-          .getElementsByClassName("example_table_verb")[0]
-          .insertAdjacentHTML(
-            "afterend",
-            `<div class="input-field inline">
-          <input id="verb_inline${this.verbLine}" type="text" className="validate" />
-          <label htmlFor="email_inline">${this.verbLine}</label>
-        </div>`
-          );
-        this.verbLine += 1;
-      }
-    },
-    addNounLine() {
-      if (this.nounLine < 3) {
-        document
-          .getElementsByClassName("example_table_noun")[0]
-          .insertAdjacentHTML(
-            "afterend",
-            `<div class="input-field inline">
-          <input id="noun_inline${this.nounLine}" type="text" className="validate" />
-          <label htmlFor="email_inline">${this.nounLine}</label>
-        </div>`
-          );
-        this.nounLine += 1;
-      }
-    },
-    addGeneralLine() {
-      if (this.generalLine < 3) {
-        document
-          .getElementsByClassName("example_table_general")[0]
-          .insertAdjacentHTML(
-            "afterend",
-            `<div class="input-field inline">
-          <input id="general_inline${this.generalLine}" type="text" className="validate" />
-          <label htmlFor="email_inline">${this.generalLine}</label>
-        </div>`
-          );
-        this.generalLine += 1;
-      }
-    },
+  components: {
+    ExpansionWordList_InterfaceElement,
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+li {
+  list-style-type: none; /* Убираем маркеры */
+}
+ul {
+  margin-left: 0; /* Отступ слева в браузере IE и Opera */
+  padding-left: 0; /* Отступ слева в браузере Firefox, Safari, Chrome */
+}
+</style>
