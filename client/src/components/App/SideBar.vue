8df77caf32d9dd71684b38daa451aa88300a259f
97Dmitry <dart_veyder_1997@mail.ru>
2021-04-28 21:50:37 +0600

    Few changed

--- client/src/components/App/SideBar.vue
+++ client/src/components/App/SideBar.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="sidebar">
-    <div class="sidebar__item" v-on:click="$router.push('/')">Home</div>
-    <div class="sidebar__item" v-on:click="$router.push('/practice')">
+    <div
+      class="sidebar__item"
+      v-on:click="$router.push('/')"
+      style="cursor: pointer"
+    >
+      Home
+    </div>
+    <div
+      class="sidebar__item"
+      v-on:click="$router.push('/practice')"
+      style="cursor: pointer"
+    >
       Practice
     </div>
-    <div class="sidebar__item" v-on:click="$router.push(`/all_words`)">
+    <div
+      class="sidebar__item"
+      v-on:click="$router.push(`/all_words`)"
+      style="cursor: pointer"
+    >
       All my study words
     </div>
-    <div class="sidebar__item" v-on:click="$router.push('/expansion')">
+    <div
+      class="sidebar__item"
+      v-on:click="$router.push('/expansion')"
+      style="cursor: pointer"
+    >
       Add new word
     </div>
     <div class="sidebar__item">Redact word</div>
