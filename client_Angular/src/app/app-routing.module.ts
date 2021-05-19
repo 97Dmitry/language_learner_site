9f035c338407e6488796f33f57799c31ef1b4960
97Dmitry <dart_veyder_1997@mail.ru>
2021-05-20 01:47:58 +0600

    Created layouts for auths pages

--- client_Angular/src/app/app-routing.module.ts
+++ client_Angular/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "login",
+        loadChildren: () =>
+          import("./auth//login/login.module").then((m) => m.LoginModule),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: "registration",
+        loadChildren: () =>
+          import("./auth/registration/registration.module").then(
+            (m) => m.RegistrationModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client_Angular/src/app/app.module.ts
+++ client_Angular/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { AuthComponent } from "./auth/auth.component";
+import { AuthLayoutComponent } from "./layouts/auth-layout/auth-layout.component";
+import { MainLayoutComponent } from "./layouts/main-layout/main-layout.component";
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, AuthLayoutComponent, MainLayoutComponent],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
