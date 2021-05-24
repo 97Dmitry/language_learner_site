1a88aeb9251828e8cc94aee43a58d371be82c176
97Dmitry <dart_veyder_1997@mail.ru>
2021-05-25 03:00:08 +0600

    Created header

--- client_Angular/src/app/shared/components/header/header.component.ts
+++ client_Angular/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { AuthService } from "src/app/service/auth.service";
 
 @Component({
   selector: "app-header",
@@ -8,7 +11,12 @@ import { Component, OnInit } from "@angular/core";
 export class HeaderComponent implements OnInit {
   username: string | null = localStorage.getItem("username");
 
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  logout(): void {
+    this.authService.logout;
+    this.router.navigate(["/login"]);
+  }
 }
