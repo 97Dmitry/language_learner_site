da16ed5f91e9a2b178089fc7c60fe6f3bec69969
97Dmitry <dart_veyder_1997@mail.ru>
2021-05-26 01:00:22 +0600

    Deleted headers because it's no longer needed

--- client_Angular/src/app/service/auth.service.ts
+++ client_Angular/src/app/service/auth.service.ts
@@ -11,32 +11,26 @@ import { tap } from "rxjs/operators";
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-    }),
-  };
+  // httpOptions = {
+  //   headers: new HttpHeaders({
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  //   }),
+  // };
 
   registration(registrationData: RegistrationForm): Observable<object> {
-    return this.http.post(
-      "api/registration",
-      registrationData,
-      this.httpOptions
-    );
+    return this.http.post("api/registration", registrationData);
   }
 
   login(loginData: LoginForm): Observable<object> {
-    return this.http
-      .post<LoginResponse>(`api/login`, loginData, this.httpOptions)
-      .pipe(
-        tap((value) => {
-          localStorage.setItem("userId", value.user.user_id);
-          localStorage.setItem("accessToken", value.tokens.accessToken);
-          localStorage.setItem("refreshToken", value.tokens.refreshToken);
-          localStorage.setItem("username", value.user.username);
-        })
-      );
+    return this.http.post<LoginResponse>(`api/login`, loginData).pipe(
+      tap((value) => {
+        localStorage.setItem("userId", value.user.user_id);
+        localStorage.setItem("accessToken", value.tokens.accessToken);
+        localStorage.setItem("refreshToken", value.tokens.refreshToken);
+        localStorage.setItem("username", value.user.username);
+      })
+    );
   }
 
   logout() {
